25f7448ec281c592e9edca17c96fee48cbe43c5c
Dominic <mrdjfraser@gmail.com>
2017-05-09 19:43:31 +0100

    dinosaur tests passing for type and noOffspring

---
+++ dinosaur.js
@@ -0,0 +1,6 @@
+var Dinosaur = function(type, noOffspring){
+  this.type = type;
+  this.noOffspring = noOffspring;
+}
+
+module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -1,1 +1,18 @@
 var assert = require("assert");
+var Dinosaur = require("../dinosaur.js")
+
+describe("Dinosaur", function(){
+
+  beforeEach(function(){
+    this.dino = new Dinosaur("Type1", 2)
+  });
+
+  it("should have a type", function(){
+    assert.equal("Type1", this.dino.type);
+  });
+
+  it("should have a no. offspring", function(){
+    assert.equal(2, this.dino.noOffspring);
+  });
+
+});

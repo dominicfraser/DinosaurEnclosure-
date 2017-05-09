5b8ed6a96ca672badd563f348a5775bd2dc0d8f1
Dominic <mrdjfraser@gmail.com>
2017-05-09 20:17:53 +0100

    find frequent breeders passing

--- enclosure.js
+++ enclosure.js
@@ -5,7 +5,27 @@ var Enclosure = function(){
 Enclosure.prototype = {
   add: function(dino){
     this.contents.push(dino);
+  },
+  removeByType: function(type){
+    for (var dino of this.contents){
+      if (dino.type === type) {
+        var pos = this.contents.indexOf(dino);
+        this.contents.splice(pos, 1);
+      };
+
+    }
+  },
+  findFeqBreeders: function(){
+    var breeders = 0;
+    for (var dino of this.contents){
+      if (dino.noOffspring > 2)
+        breeders++
+    }
+    return breeders;
   }
 }
+
+
+
 
 module.exports = Enclosure;
--- specs/enclosure_spec.js
+++ specs/enclosure_spec.js
@@ -5,19 +5,50 @@ var Enclosure = require("../enclosure.js")
 describe("Enclosure", function(){
 
   beforeEach(function(){
-    this.dino1 = new Dinosaur("Type1", 2)
-    this.dino2 = new Dinosaur("Type1", 2)
-    this.dino3 = new Dinosaur("Type2", 3)
+    this.dino1 = new Dinosaur("Type1", 2);
+    this.dino2 = new Dinosaur("Type1", 2);
+    this.dino3 = new Dinosaur("Type2", 3);
+    this.dino4 = new Dinosaur("Type2", 3);
     this.enclosure1 = new Enclosure();
   });
 
   it("should start empty", function(){
     assert.equal(0, this.enclosure1.contents.length)
-  })
+  });
 
   it("should be able to add one dinosaur", function(){
     this.enclosure1.add(this.dino1);
     assert.equal(1, this.enclosure1.contents.length)
-  })
+  });
+
+  it("should be able to add two dinosaurs", function(){
+    this.enclosure1.add(this.dino1);
+    this.enclosure1.add(this.dino2);
+    assert.equal(2, this.enclosure1.contents.length)
+  });
+
+  it("should be able to remove one by type", function(){
+    this.enclosure1.add(this.dino1);
+    this.enclosure1.add(this.dino3);
+    this.enclosure1.removeByType("Type1");
+    assert.equal(1, this.enclosure1.contents.length)
+  });
+
+  it("should be able to remove two by type", function(){
+    this.enclosure1.add(this.dino1);
+    this.enclosure1.add(this.dino2);
+    this.enclosure1.add(this.dino3);
+    this.enclosure1.removeByType("Type1");
+    assert.equal(1, this.enclosure1.contents.length)
+  });
+
+  it("should find frequent breeders", function(){
+    this.enclosure1.add(this.dino1);
+    this.enclosure1.add(this.dino2);
+    this.enclosure1.add(this.dino3);
+    this.enclosure1.add(this.dino4);
+
+    assert.equal(2, this.enclosure1.findFeqBreeders())
+  });
 
 });
